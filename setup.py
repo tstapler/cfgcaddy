498de8a51665c8fc17edd732acfb1a83f1f3466c
Tyler Stapler <tstapler@iastate.edu>
2016-06-20 23:07:46 -0500

    Adds a setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stapler-linker',
+      version='0.1',
+      description='Tool for managing dotfiles',
+      url='https://github.com/tstapler/stapler-linker',
+      author='Tyler Stapler',
+      author_email='tystapler at gm@il',
+      license='MIT',
+      packages=['stapler-linker'],
+      zip_safe=True)
