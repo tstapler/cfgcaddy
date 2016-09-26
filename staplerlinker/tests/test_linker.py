79b10366c99f550bb2f2b59d204221c363222dae
Tyler Stapler <tyler.stapler@workiva.com>
2016-09-26 08:57:29 -0500

    Add initial test for linker

--- staplerlinker/tests/test_linker.py
+++ staplerlinker/tests/test_linker.py
@@ -1,2 +1,8 @@
 from staplerlinker.tests import (list_files, create_files_from_tree,
                                  FileLinkTestCase)
+
+
+class TestLinker(FileLinkTestCase):
+    def test_customlinks_file_absent(self):
+        """Test when the custom links file doesn't exist"""
+        pass
