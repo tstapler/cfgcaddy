9d1ab4dbcab572bb6a06fc3a3a78870342be27b2
Tyler Stapler <tystapler@gmail.com>
2017-12-11 14:53:15 -0600

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cfgcaddy',
-      version='0.1',
+      version='0.0.2',
       description='Tool for managing configuration files',
       url='https://github.com/tstapler/cfgcaddy',
       author='Tyler Stapler',
