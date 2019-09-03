0c2c432d5b6ac657d17f38e52f30e2a77e4e5b8e
Tyler Stapler <tystapler@gmail.com>
2019-09-03 11:49:52 -0500

    Pin requirements and bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cfgcaddy',
-    version='0.1.1',
+    version='0.1.2',
     description='Tool for managing configuration files',
     url='https://github.com/tstapler/cfgcaddy',
     author='Tyler Stapler',
@@ -10,11 +10,11 @@ setup(
     license='MIT',
     packages=['cfgcaddy'],
     install_requires=[
-        "click",
-        "enum34",
-        "pathspec",
-        "pyyaml",
-        "whaaaaat",
+        "click==6.7",
+        "enum34==1.1.6",
+        "pathspec==0.5.3",
+        "pyyaml==3.12",
+        "whaaaaat==0.5.2",
     ],
     entry_points={'console_scripts': ['cfgcaddy = cfgcaddy.__main__:main']},
     zip_safe=True)
